3a4f9f3eff5a818ede294d534634cdebbba96f96
queeniemendijar <26ur0211@psu.edu.ph>
2026-09-02 21:16:58 +0800

    Add files via upload

---
+++ Assignment3 (1).java
@@ -0,0 +1,25 @@
+class Assignment3 {
+    //main method
+    public static void main(String[] args) {
+        //store data in a string variable,
+        // string literals are represented by multiple character and are enclosed by double qoutes
+        String firstName = "Queenie",
+                lastName = "Mendijar",
+                address = "Malasiqui",
+                mobileNumber = "09512713962";
+        //used to declare a variable that can only store integers
+        int age = 18;
+        //data type used to store numbers with fraction and decimal points
+        double gwa = 92.0;
+
+        //used to display messages
+        System.out.println(
+                "Firstname: " + firstName + "\n" +
+                        // represents a newline character
+                        "Lastname: " + lastName + "\n" +
+                        "Address: " + address + "\n" +
+                        "Mobile Number: " + mobileNumber + "\n" +
+                        "Age: " + age + "\n" +
+                        "Gwa: " + gwa);
+    }
+}
